77e112fb90992049c19f311b736d744d8ae6d03d
Jeremy Audet <jerebear@protonmail.com>
2019-04-19 18:44:50 -0400

    Add sanity-checking Keras-based neural network

---
+++ keras/test-01/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# coding=utf-8
+"""A very simple Keras-based NN, for sanity checking.
+
+Depends on the following pure-Python packages:
+
+*   keras
+*   tensorflow
+"""
+import numpy as np
+from keras import layers, losses, models, optimizers, utils
+
+
+def main():
+    """Execute business logic."""
+    # Create model.
+    model = models.Sequential((
+        layers.Dense(32, input_shape=(128,)),
+        layers.Activation('relu'),
+        layers.Dense(10),
+        layers.Activation('softmax'),
+    ))
+    model.compile(
+        optimizer=optimizers.SGD(),
+        loss=losses.mean_squared_error,
+        metrics=['accuracy'],
+    )
+
+    # Generate bogus data.
+    data = np.random.random((1024, 128))
+    labels = np.random.randint(10, size=(1024, 1))
+
+    # Convert labels to categorical one-hot encoding.
+    one_hot_labels = utils.to_categorical(labels, num_classes=10)
+
+    # Train the model, iterating on the data in batches of 32 samples.
+    model.fit(data, one_hot_labels, epochs=1000, batch_size=32)
+
+
+if __name__ == '__main__':
+    main()
