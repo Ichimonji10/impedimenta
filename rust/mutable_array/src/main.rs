2d41618972ca8182d716f8f02e310cd4f152f203
Jeremy Audet <jerebear@protonmail.com>
2019-05-01 18:38:31 -0400

    Polish existing Rust projects
    Ensure all existing Rust projects work as intended. Accept autogenerated
    updates to cargo lock files. Add .gitignore files to ignore `/target/`
    directories.

--- rust/mutable_array/src/main.rs
+++ rust/mutable_array/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     v.push(40);
     {
         let mut v2 : &Vec<i32> = &mut v;
-        v2[0] = v2[0] + 1; // comment out to let program run
+        v2[0] += 1; // comment out to let program run
     }
     v[0] = v[0] + 1;
     println!("{}", v[0]);
