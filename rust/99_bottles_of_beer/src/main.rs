8232b35aef7d196e862c9cffca967520cf8c36c2
Jeremy Audet <jerebear@protonmail.com>
2019-05-02 11:39:52 -0400

    Reformat existing Rust code with rustfmt

--- rust/99_bottles_of_beer/src/main.rs
+++ rust/99_bottles_of_beer/src/main.rs
@@ -5,19 +5,22 @@ fn main() {
         if i > 2 {
             println!(
                 "Take one down and pass it around, {} more bottles of beer on \
-                the wall!", i - 1
+                 the wall!",
+                i - 1
             );
         } else if i == 2 {
             println!(
                 "Take one down and pass it around, one more bottle of beer on \
-                the wall!"
+                 the wall!"
             );
         } else if i == 1 {
             println!(
                 "Take one down and pass it around, no more bottles of beer \
-                on the wall!"
+                 on the wall!"
             );
         }
-        if i > 1 { println!(""); }
+        if i > 1 {
+            println!("");
+        }
     }
 }
--- rust/mutable_array/src/main.rs
+++ rust/mutable_array/src/main.rs
@@ -1,10 +1,10 @@
 // From: http://www.electronicdesign.com/industrial/rust-and-spark-software-reliability-everyone
 
 fn main() {
-    let mut v : Vec<i32> = Vec::new();
+    let mut v: Vec<i32> = Vec::new();
     v.push(40);
     {
-        let mut v2 : &Vec<i32> = &mut v;
+        let mut v2: &Vec<i32> = &mut v;
         v2[0] += 1; // comment out to let program run
     }
     v[0] = v[0] + 1;
