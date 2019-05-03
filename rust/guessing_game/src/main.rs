585d3cb8d107c31eb60aa8e6f4d3e4169f3e1acf
Jeremy Audet <jerebear@protonmail.com>
2019-05-02 21:22:51 -0400

    Rewrite Rust guessing game
    Use several tools, most notably `Result<T, E>`, to more gracefully
    handle undesirable inputs.

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -5,11 +5,15 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io::stdin;
 
+pub const MIN: i32 = 1;
+pub const MAX: i32 = 100;
+
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(MIN, MAX + 1);
     println!(
         "Welcome to the guessing game! Your objective is to guess a randomly \
-         generated number."
+         generated number. The number is between {} and {}, inclusive.",
+        MIN, MAX
     );
     loop {
         // Fetch a guess from the user.
@@ -18,16 +22,22 @@ fn main() {
         stdin()
             .read_line(&mut str_guess)
             .expect("Failed to read line.");
-        let guess: u32 = match str_guess.trim().parse() {
-            Ok(num) => num,
+        let guess: Guess = match str_guess.trim().parse() {
             Err(msg) => {
-                println!("Whoops! Input isn't a number. (Error: {})", msg);
+                println!("Error: {}", msg);
                 continue;
             }
+            Ok(int_guess) => match Guess::new(int_guess) {
+                Err(msg) => {
+                    println!("Error: {}", msg);
+                    continue;
+                }
+                Ok(guess) => guess,
+            },
         };
 
         // Compare the user's guess to the secret_number
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small."),
             Ordering::Equal => {
                 println!("Just right.");
@@ -35,5 +45,26 @@ fn main() {
             }
             Ordering::Greater => println!("Too large."),
         }
+    }
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Result<Guess, String> {
+        if value < MIN || value > MAX {
+            Err(format!(
+                "Guess value must be between 1 and 100 inclusive, got {}.",
+                value
+            ))
+        } else {
+            Ok(Guess { value })
+        }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
     }
 }
